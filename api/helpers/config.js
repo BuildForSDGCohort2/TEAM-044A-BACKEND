d3705c16f0c19dc4097e7aa2cef15f9ef1f53b57
Iyosayi <Iyosa14@gmail.com>
2020-10-03 10:22:11 +0100

    fixed cloudamqp

--- api/helpers/config.js
+++ api/helpers/config.js
@@ -1,6 +1,3 @@
-const AMQP_URI =
-  process.env.NODE_ENV === 'production'
-    ? process.env.CLOUDAMQP_URL
-    : 'amqp://localhost'
+const AMQP_URI = process.env.CLOUDAMQP_URL || 'amqp://localhost'
 
 export default AMQP_URI
